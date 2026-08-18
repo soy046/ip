5e57fefb85deaa9fceff473185108d47e13596b8
soy046 <chenyuzhen2007@outlook.com>
2026-08-18 09:36:31 +0800

    Change banner style

--- src/main/java/Tuesday.java
+++ src/main/java/Tuesday.java
@@ -1,6 +1,13 @@
 public class Tuesday {
     public static void main(String[] args) {
-        String banner = "Tuesday\n";
+        String banner = " _____                     _             \n"
+                + "|_   _|                   | |            \n"
+                + "  | | _   _  ___  ___   __| | __ _ _   _\n"
+                + "  | || | | |/ _ \\/ __| / _` |/ _` | | | |\n"
+                + "  | || |_| |  __/\\__ \\| (_| | (_| | |_| |\n"
+                + "  |_| \\__,_|\\___||___/ \\__,_|\\__,_|\\__, |\n"
+                + "                                     __/ |\n"
+                + "                                    |___/";
         System.out.println(banner);
     }
 }
