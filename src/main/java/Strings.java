e5ddbe3c031c79758af60d57cae51f1f5cbe63b0
soy046 <chenyuzhen2007@outlook.com>
2026-08-18 09:59:04 +0800

    Move all the strings to Strings.java

---
+++ src/main/java/Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+    public static final String banner = " _____                     _             \n"
+            + "|_   _|                   | |            \n"
+            + "  | | _   _  ___  ___   __| | __ _ _   _\n"
+            + "  | || | | |/ _ \\/ __| / _` |/ _` | | | |\n"
+            + "  | || |_| |  __/\\__ \\| (_| | (_| | |_| |\n"
+            + "  |_| \\__,_|\\___||___/ \\__,_|\\__,_|\\__, |\n"
+            + "                                     __/ |\n"
+            + "                                    |___/";
+    public static final String greeting = "Hello! I'm Tuesday.\n" +
+            "What can I do for you?";
+    public static final String farewell = "Bye. Hope to see you again soon!";
+    public static final String horizontalLine = "____________________________________________________________";
+}
--- src/main/java/Tuesday.java
+++ src/main/java/Tuesday.java
@@ -1,25 +1,10 @@
 public class Tuesday {
     public static void main(String[] args) {
-        // below are the Strings needed
-        String banner = " _____                     _             \n"
-                + "|_   _|                   | |            \n"
-                + "  | | _   _  ___  ___   __| | __ _ _   _\n"
-                + "  | || | | |/ _ \\/ __| / _` |/ _` | | | |\n"
-                + "  | || |_| |  __/\\__ \\| (_| | (_| | |_| |\n"
-                + "  |_| \\__,_|\\___||___/ \\__,_|\\__,_|\\__, |\n"
-                + "                                     __/ |\n"
-                + "                                    |___/";
-        String greeting = "Hello! I'm Tuesday.\n" +
-                "What can I do for you?";
-        String farewell = "Bye. Hope to see you again soon!";
-        String horizontalLine = "____________________________________________________________";
-
-        // below are the outputs
-        System.out.println(horizontalLine);
-        System.out.println(banner);
-        System.out.println(greeting);
-        System.out.println(horizontalLine);
-        System.out.println(farewell);
-        System.out.println(horizontalLine);
+        System.out.println(Strings.horizontalLine);
+        System.out.println(Strings.banner);
+        System.out.println(Strings.greeting);
+        System.out.println(Strings.horizontalLine);
+        System.out.println(Strings.farewell);
+        System.out.println(Strings.horizontalLine);
     }
 }
