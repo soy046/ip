ba70939f5d6601aa060c89a1afc70d356f34a752
soy046 <chenyuzhen2007@outlook.com>
2026-08-18 09:45:19 +0800

    Make the chatbot simply greet and exit

--- src/main/java/Tuesday.java
+++ src/main/java/Tuesday.java
@@ -1,5 +1,6 @@
 public class Tuesday {
     public static void main(String[] args) {
+        // below are the Strings needed
         String banner = " _____                     _             \n"
                 + "|_   _|                   | |            \n"
                 + "  | | _   _  ___  ___   __| | __ _ _   _\n"
@@ -8,6 +9,17 @@ public class Tuesday {
                 + "  |_| \\__,_|\\___||___/ \\__,_|\\__,_|\\__, |\n"
                 + "                                     __/ |\n"
                 + "                                    |___/";
+        String greeting = "Hello! I'm Tuesday.\n" +
+                "What can I do for you?";
+        String farewell = "Bye. Hope to see you again soon!";
+        String horizontalLine = "____________________________________________________________";
+
+        // below are the outputs
+        System.out.println(horizontalLine);
         System.out.println(banner);
+        System.out.println(greeting);
+        System.out.println(horizontalLine);
+        System.out.println(farewell);
+        System.out.println(horizontalLine);
     }
 }
