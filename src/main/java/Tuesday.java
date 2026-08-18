921f7c519099935ab3d5f819657d61d910a5eb44
soy046 <chenyuzhen2007@outlook.com>
2026-08-18 11:26:37 +0800

    Allow task adding

--- src/main/java/Tuesday.java
+++ src/main/java/Tuesday.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Tuesday {
     public static void main(String[] args) {
-        // initialize the input and scanner
+        // initialize the input, scanner and the task array with the index for input
         Scanner sc = new Scanner(System.in);
         String input = "";
+        String[] taskArray = new String[100];
+        int index = 0;
 
         // greetings part
         System.out.println(Strings.horizontalLine);
@@ -12,11 +14,13 @@ public class Tuesday {
         System.out.println(Strings.greeting);
         System.out.println(Strings.horizontalLine);
 
-        // echoing part
+        // task adding part
         input = sc.nextLine();
         while (! input.equals("bye")) {
+            taskArray[index] = input;
+            index++;
             System.out.println(Strings.horizontalLine);
-            System.out.println(input);
+            System.out.println("added: " + input);
             System.out.println(Strings.horizontalLine);
             input = sc.nextLine();
         }
